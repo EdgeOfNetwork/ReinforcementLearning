29519b8a0099cc7961fbfb0086a6726063f4276a
lsy <qhrrl4556@gmail.com>
2023-03-05 21:40:45 +0900

    ch3

--- StableBaseline/ch3/cartpole.py
+++ StableBaseline/ch3/cartpole.py
@@ -1,5 +1,42 @@
 import gym
+import matplotlib.pyplot as plt
+
 
 env = gym.make("CartPole-v0")
+# render = lambda: plt.imshow(env.render(mode = 'rgb_array'))
+# env.reset()
+#
+# for _ in range(100):
+#     env.step(env.action_space.sample())
+#     env.render()
+#
+# env.close()
+#
+# print(env.observation_space)
+# print(env.observation_space.shape)
+# print(env.action_space)
+#
+# print(env.reset())
+# print(env.observation_space.high)
+# print(env.observation_space.low)
 
-env.render()
+
+num_episodes = 100
+num_timesteps = 50
+
+for i in range(num_episodes):
+
+    Return = 0
+    state = env.reset()
+
+    for t in range(num_timesteps):
+        random_action = env.action_space.sample()
+        next_state, reward, done, _ = env.step(random_action)
+        Return += reward
+        if done:
+            break
+
+    if i % 20 == 0:
+        print('Episode : {}, Return : {}'.format(i,Return))
+
+env.close()
